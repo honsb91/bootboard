962128d19db3e9144d37787ebe2c3339cb32ba69
honsb91 <114128759+honsb91@users.noreply.github.com>
2024-06-18 12:10:10 +0900

    2024.06.18(게시글 수정 테스트까지)

--- src/main/java/com/example/bootboard/service/BoardService.java
+++ src/main/java/com/example/bootboard/service/BoardService.java
@@ -1,12 +1,26 @@
 package com.example.bootboard.service;
 
 import com.example.bootboard.dto.BoardDTO;
+import com.example.bootboard.dto.PageRequestDTO;
+import com.example.bootboard.dto.PageResultDTO;
 import com.example.bootboard.entity.Board;
 import com.example.bootboard.entity.Member;
 
 public interface BoardService {
 
     Long register(BoardDTO dto);
+
+    //목록처리 ↓
+    PageResultDTO<BoardDTO, Object[]> getList(PageRequestDTO pageRequestDTO);
+
+    //조회처리
+    BoardDTO get(Long bno);
+
+    //삭제처리
+    void removeWithReplies(Long bno);
+
+    //수정처리
+    void modify(BoardDTO boardDTO);
 
     default Board dtoToEntity(BoardDTO dto){
         Member member = Member.builder().email(dto.getWriterEmail()).build();
@@ -18,4 +32,21 @@ public interface BoardService {
                 .build();
         return board;
     }
+
+    default BoardDTO entityToDTO(Board board, Member member, Long replyCount){
+
+        BoardDTO boardDTO = BoardDTO.builder()
+                .bno(board.getBno())
+                .title(board.getTitle())
+                .content(board.getContent())
+                .regDate(board.getRegDate())
+                .modDate(board.getModDate())
+                .writerEmail(member.getEmail())
+                .writerName(member.getName())
+                .replyCount(replyCount.intValue()) // Long으로 나오므로 int로 처리하도록
+                .build();
+        return boardDTO;
+    }
+
+
 }
--- src/test/java/com/example/bootboard/service/BoardServiceTests.java
+++ src/test/java/com/example/bootboard/service/BoardServiceTests.java
@@ -1,6 +1,8 @@
 package com.example.bootboard.service;
 
 import com.example.bootboard.dto.BoardDTO;
+import com.example.bootboard.dto.PageRequestDTO;
+import com.example.bootboard.dto.PageResultDTO;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -22,4 +24,41 @@ public class BoardServiceTests {
         Long bno = service.register(dto);
     }
 
+    @Test
+    public void testList(){
+        PageRequestDTO pageRequestDTO = new PageRequestDTO();
+
+        PageResultDTO<BoardDTO, Object[]> result = service.getList(pageRequestDTO);
+
+        for (BoardDTO boardDTO : result.getDtoList()){
+            System.out.println(boardDTO);
+        }
+    }
+
+    //조회 TEST
+    @Test
+    public void testGet(){
+        Long bno = 100L;
+        BoardDTO boardDTO = service.get(bno);
+        System.out.println(boardDTO);
+    }
+
+    // 삭제 TEST
+    @Test
+    public void testRemove(){
+        Long bno = 26L;
+        service.removeWithReplies(bno);
+    }
+
+    // 수정 TEST
+    @Test
+    public void testModify(){
+        BoardDTO boardDTO = BoardDTO.builder()
+                .bno(2L)
+                .title("제목 변경합니다.")
+                .content("내용 변경합니다.")
+                .build();
+
+        service.modify(boardDTO);
+    }
 }
